14545829d6dc0bc9ebfe7c53d90ceaaa7bbdaee8
Administrator <Administrator@PC248>
2020-06-15 09:59:15 +0700

    f

---
+++ lab456/Migrations/202006150251535_AddNameColumnToApplicationUser.cs
@@ -0,0 +1,16 @@
+namespace lab456.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameColumnToApplicationUser : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
